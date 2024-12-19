afe0cfc29a2b29d8a315f8bdfe674a4cbdc202df
Rodrigo Aguiar <86214343+RodrigoAguiarS@users.noreply.github.com>
2024-12-18 23:23:40 -0300

    adiciona componentes de criação para Cargo e Funcionário, incluindo serviços

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+
+import { PerfilCreateComponent } from './components/perfil/perfil-create/perfil-create.component';
+import { PerfilListComponent } from './components/perfil/perfil-list/perfil-list.component';
+import { PerfilUpdateComponent } from './components/perfil/perfil-update/perfil-update.component';
+import { PerfilDeleteComponent } from './components/perfil/perfil-delete/perfil-delete.component';
+
+import { CargoCreateComponent } from './components/cargo/cargo-create/cargo-create.component';
+import { FuncionarioCreateComponent } from './components/funcionario/funcionario-create/funcionario-create.component';
+
 import { NoAuthGuard } from './auth/noauth.guard';
 import { RoleGuard } from './auth/role.guard';
 import { HomeComponent } from './components/home/home.component';
@@ -20,6 +29,29 @@ const routes: Routes = [
         component: HomeComponent,
         canActivate: [RoleGuard],
         data: { roles: [ACESSO.ADMINISTRADOR, ACESSO.USUARIO] },
+      },
+      {
+        path: 'funcionarios',
+        component: FuncionarioCreateComponent,
+        canActivate: [RoleGuard],
+        data: { roles: [ACESSO.ADMINISTRADOR] },
+      },
+      {
+        path: 'perfis',
+        canActivate: [RoleGuard],
+        data: { roles: [ACESSO.ADMINISTRADOR] },
+        children: [
+          { path: '', component: PerfilListComponent },
+          { path: 'create', component: PerfilCreateComponent },
+          { path: 'update/:id', component: PerfilUpdateComponent },
+          { path: 'delete/:id', component: PerfilDeleteComponent },
+        ],
+      },
+      {
+        path: 'cargos',
+        component: CargoCreateComponent,
+        canActivate: [RoleGuard],
+        data: { roles: [ACESSO.ADMINISTRADOR] },
       }
     ]
   }
